568aee1571e372436fbe6ec960308a74b4f11c4f
Ramon William <roniewill@gmail.com>
2017-11-21 20:27:12 -0300

    Concatenação e como utilizar prompt para entrada de dados

---
+++ concatenation.js
@@ -0,0 +1,17 @@
+/*
+    Concatenação é um termo usado em computação para designar a operação de unir o conteúdo de duas strings. 
+    Por exemplo, considerando as strings "casa" e "mento" a concatenação da primeira com a segunda gera a string "casamento".
+    Source: https://pt.wikipedia.org/wiki/Concatena%C3%A7%C3%A3o
+*/
+
+var nome = 'João';
+var idade = 40;
+var profissao = 'Administrador';
+
+/*
+    Operador de concatenação: +
+    Vamos formar uma frase unindo essas variáveis com um string
+*/ 
+document.write('Olá ' + nome + ', você tem ' + idade + ' de idade e sua profissão é ' + profissao);
+
+// Você pode testar esse código em: https://jsfiddle.net/
---
+++ data-input-output.js
@@ -0,0 +1,15 @@
+/*
+    Vamos utilizar a função prompt do JS para capturar o valor e mostrar na página do seu navegador
+*/
+
+var nome = prompt('Digite seu nome');
+var idade = prompt('Digite sua idade');
+var profissao = prompt('Digite sua profissão');
+
+/*
+    Operador de concatenação: +
+    Vamos formar uma frase unindo essas variáveis com um string
+*/ 
+document.write('Olá ' + nome + ', você tem ' + idade + ' de idade e sua profissão é ' + profissao);
+
+// Você pode testar esse código em: https://jsfiddle.net/
