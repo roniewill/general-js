81cbfd0163d1b8dae80fb1131075e213e5e172e5
Ramon William <roniewill@gmail.com>
2017-11-21 19:06:50 -0300

    Comentários em javascript

---
+++ comments.js
@@ -0,0 +1,11 @@
+/*
+    Seu comentário aqui 
+*/
+
+var notaProva1 = 7;
+var notaProva2 = 8;
+var notaProva3 = 6.5;
+
+// Seu comentário aqui
+var media = (notaProva1 + notaProva2 + notaProva3) / 3;
+document.write('A média é: ' + media);
