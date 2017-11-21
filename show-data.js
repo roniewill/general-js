fca6f0823bad9623d154f46342282c99048cc3ed
Ramon William <roniewill@gmail.com>
2017-11-21 20:10:48 -0300

    Exibindo dados com funções do JS

---
+++ show-data.js
@@ -0,0 +1,30 @@
+/*
+    Mostrar dados em javascript
+*/
+
+var _inteiro = 8;
+
+var _valorFracionado = 4.5;
+
+var _texto = 'Alguma coisa';
+
+/*
+    Nessa função você pode passar como argumento/parametro sua variável
+    o mesmo se encarregará de mostrar o valor de sua variável em um popup
+*/
+alert(_texto);
+
+/*
+    Nessa função você pode passar como argumento/parametro sua variável
+    o mesmo se encarregará de mostrar o valor de sua variável dentro do corpo na página do seu navegador
+*/
+document.write(_texto);
+
+/*
+    Nessa função você pode passar como argumento/parametro sua variável
+    o mesmo se encarregará de mostrar o valor de sua variável no console de seu navegador
+    Obs.: Ctrl+Shift+l
+*/
+console.log(_texto);
+
+// Você pode testar esse código em: https://jsfiddle.net/
