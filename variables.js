731e179471f669762c56eb94a7da1114693c776a
Ramon William <roniewill@gmail.com>
2017-11-21 19:46:14 -0300

    Variáveis e tipos de variáveis em JS

---
+++ variables.js
@@ -0,0 +1,42 @@
+/*
+    Variáveis: são endereços que apontam para valores que estão armazenados
+    temporáriamente em memória
+*/
+
+/* 
+    Tipos de variáveis:
+    Int: armazena valores inteiros como: -9 ou 9
+    Float: armazena valores fracionados como: -7.5 ou 4.5
+    String: armazena textos
+    Boolean: retorna um estado, verdadeiro(true) ou falso(false)
+
+    Regras para declaração de variáveis:
+    * Não podem iniciar  com números
+    * Não podem utilizar caracteres especiais
+    * Não podem utilizar palavras reservadas do Javascript
+
+    Javascript é Case-sensitive:
+    curso é diferente Curso ou mesmo de CURSO
+
+    Comando var para declaração de variáveis:
+
+    Regras para declaração de variáveis:
+    Jeito certo: 
+        var _123MyVar;
+        var MyVar;
+        var myvar;
+        var MYVAR;
+    
+    Errado:
+        var 123var;
+        var número;
+*/
+
+// MÃO NA MASSA!
+
+var _inteiro = 8;
+var _valorFracionado = 4.5;
+var _texto = 'Alguma coisa';
+var _verdadeiroOuFalco = true; // posso atribuir tanto true como false
+
+// Obs.: o tipo de variável é definido na atribuição do valor
