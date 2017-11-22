6c983bc09dc2e66d98b50c085714082a215a5118
Ramon William <roniewill@gmail.com>
2017-11-22 18:01:40 -0300

    conditionals with comparison operators

---
+++ if-else-comparison-operator.js
@@ -0,0 +1,19 @@
+/*
+    Let's work with conditionals and comparison operators
+
+    Comparison operators for test:
+    ==
+    ===
+    !=
+    !==
+    <
+    >
+    <=
+    >=
+*/
+
+if(2 == 3){
+    document.write('True');
+} else {
+    document.write('False');
+}
