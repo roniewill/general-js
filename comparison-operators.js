47f50c583853f7b8d2bb741aeb5775cedb351370
Ramon William <roniewill@gmail.com>
2017-11-21 23:04:29 -0300

    Comparison operators

---
+++ comparison-operators.js
@@ -0,0 +1,39 @@
+/*
+    JavaScript has both strict and type–converting comparisons. A strict comparison (e.g., ===) is only true if the operands are of the same type and the contents match. 
+    The more commonly-used abstract comparison (e.g. ==) converts the operands to the same type before making the comparison. 
+    For relational abstract comparisons (e.g., <=), the operands are first converted to primitives, then to the same type, before comparison.
+
+    Javascript has 8 comparison operators:
+
+    1- Equality (==)
+    Syntax:
+        x == y
+    
+    2 - Inequality (!=)
+    Sytax:
+        x != y
+
+    3 - Identity / strict equality (===)
+    Sytax:
+        x === y
+
+    4 - Non-identity / strict inequality (!==)
+    Syntax:
+        x !== y
+
+    5 - Greater than operator (>)
+    Syntax:
+        x > y
+
+    6 - Greater than or equal operator (>=)
+    Syntax:
+        x >= y
+
+    7 - Less than operator (<)
+    Syntax:
+        x < y
+
+    8 - Less than or equal operator (<=)
+    Syntax:
+        x <= y
+*/
