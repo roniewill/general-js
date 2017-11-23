39d403a08772222146ddc4fee6c85c463dec8c5e
Ramon William <roniewill@gmail.com>
2017-11-22 22:57:28 -0300

    Working with funciotns

---
+++ functions.js
@@ -0,0 +1,4 @@
+/*
+    A function is a code snippet that can be called by other code or by itself, or a variable that refers to the function. 
+    When a function is called, arguments are passed to the function as input, and the function can optionally return an output. A function in JavaScript is also an object.
+*/
