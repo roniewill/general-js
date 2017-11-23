2758fc26165561811607071e39abb576da8143bf
Ramon William <roniewill@gmail.com>
2017-11-22 21:16:13 -0300

    Arithmetic operators

---
+++ arithmetic-operators.js
@@ -0,0 +1,79 @@
+/*
+    Arithmetic operators take numerical values (either literals or variables) as their operands and return a single numerical value. 
+    The standard arithmetic operators are addition (+), subtraction (-), multiplication (*), and division (/).
+
+    1 - Addition (+)
+
+        The addition operator produces the sum of numeric operands or string concatenation.
+
+        Syntax:
+
+        Operator: x + y
+    
+    2 - Subtraction (-)
+
+        The subtraction operator subtracts the two operands, producing their difference.
+
+        Syntax:
+
+        Operator: x - y
+    
+    3 - Division (/)
+
+        The division operator produces the quotient of its operands where the left operand is the dividend and the right operand is the divisor.
+
+        Syntax:
+
+        Operator: x / y
+    
+    4 - Multiplication (*)
+
+        The multiplication operator produces the product of the operands.
+
+        Syntax:
+
+        Operator: x * y
+    
+    5 - Remainder (%)
+
+        The remainder operator returns the remainder left over when one operand is divided by a second operand. 
+        It always takes the sign of the dividend, not the divisor. It uses a built-in modulo function to produce the result, which is the integer remainder of dividing var1 by var2 — for example — var1 modulo var2. 
+        There is a proposal to get an actual modulo operator in a future version of ECMAScript, the difference being that the modulo operator result would take the sign of the divisor, not the dividend.
+
+        Syntax:
+
+        Operator: var1 % var2
+
+    6 - Exponentiation (**)
+
+        The exponentiation operator returns the result of raising first operand to the power second operand. 
+        that is, var1var2, in the preceding statement, where var1 and var2 are variables. Exponentiation operator is right associative. a ** b ** c is equal to a ** (b ** c).
+
+        Syntax:
+
+        Operator: var1 ** var2
+    
+    7 - Increment (++)
+
+        The increment operator increments (adds one to) its operand and returns a value.
+
+        * If used postfix, with operator after operand (for example, x++), then it returns the value before incrementing.
+        * If used prefix with operator before operand (for example, ++x), then it returns the value after incrementing.
+        Syntax:
+
+        Operator: x++ or ++x
+    
+    8 - Decrement (--)
+
+        The decrement operator decrements (subtracts one from) its operand and returns a value.
+
+        * If used postfix (for example, x--), then it returns the value before decrementing.
+        * If used prefix (for example, --x), then it returns the value after decrementing.
+        Syntax:
+
+        Operator: x-- or --x
+*/
+
+
+
+// you can test this code in: https://jsfiddle.net/
